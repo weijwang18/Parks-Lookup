88b116a2411435ca8ea5f5fe2326173b6209c8dc
weijwang18@gmail.com <weijwang18@gmail.com>
2022-08-19 12:32:34 -0700

    add park model

---
+++ ParkFinder/Models/Park.cs
@@ -0,0 +1,9 @@
+namespace ParkFinder.Models
+{
+    public class Park
+    {
+        public int ParklId { get; set; }
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
