7dad2246896d068b38850c7d2f99c0d71219677a
weijwang18@gmail.com <weijwang18@gmail.com>
2022-08-19 14:14:23 -0700

    add CRUD

--- ParkFinder/Controllers/ParksController.cs
+++ ParkFinder/Controllers/ParksController.cs
@@ -23,13 +23,74 @@ namespace ParkFinder.Controllers
       return await _db.Parks.ToListAsync();
     }
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Park>> GetPark(int id)
+    {
+        var park = await _db.Parks.FindAsync(id);
+
+        if (park == null)
+        {
+            return NotFound();
+        }
+
+        return park;
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Park park)
+    {
+      if (id != park.ParkId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(park).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!ParkExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+       return NoContent();
+    }
+
     [HttpPost]
     public async Task<ActionResult<Park>> Post(Park park)
     {
       _db.Parks.Add(park);
       await _db.SaveChangesAsync();
 
-      return CreatedAtAction("Post", new { id = park.ParkId }, park);
+       return CreatedAtAction(nameof(GetPark), new { id = park.ParkId }, park);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id)
+    {
+      var park = await _db.Parks.FindAsync(id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+
+    private bool ParkExists(int id)
+    {
+      return _db.Parks.Any(e => e.ParkId == id);
     }
   }
 }
